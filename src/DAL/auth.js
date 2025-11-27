d0f6a63e9bf3d801fa076e88cc89259ce8dd81d4
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-11-27 18:13:31 +0500

    update api

--- src/DAL/auth.js
+++ src/DAL/auth.js
@@ -3,7 +3,7 @@ import { invokeApi } from "../Utils/InvokeApi";
 export const login = async (formData) => {
   console.log("Coming data is ", ...formData);
   const reqObj = {
-    path: "/admin/login",
+    path: "/api/login",
     method: "POST",
     headers: {},
     postData: formData,
